571217885a090a020c50ad2fca4917ccda2f4f62
Paul Chaignon <paul.chaignon@gmail.com>
2014-10-17 18:09:20 -0400

    First version toward a proof of concept for a covert channel in Git hashes

---
+++ git_hashes/git_hashes.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys;
+import string;
+import hashlib;
+import time;
+
+
+def increment(string):
+	new_string = ''
+	all_1 = True
+	for bit in string:
+		if bit == '0':
+			all_1 = False
+			break
+	if all_1:
+		new_string += '1'
+		for bit in string:
+			new_string += '0'
+	else:
+		new_string = '{:0{}b}'.format(int(string, 2) + 1, len(string))
+	return new_string
+
+
+
+def to_spaces(string):
+	spaces = "";
+	for bit in string:
+		if bit == '0':
+			spaces += " "
+		else:
+			spaces += "	"
+	return spaces
+
+
+comment = sys.argv[1]
+message = sys.argv[2]
+offset = 0
+if len(sys.argv) > 3:
+	offset = int(sys.argv[3])
+
+sha1 = str(hashlib.sha1(comment.encode('utf-8')).hexdigest())
+print(sha1)
+
+start_time = time.time()
+spaces = '0';
+while sha1[offset: offset + len(message)] != message:
+	spaces = increment(spaces)
+	sha1 = hashlib.sha1((comment + to_spaces(spaces)).encode('utf-8')).hexdigest()
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print(spaces)
+print(comment + to_spaces(spaces) + "EOL")
+print(sha1)
