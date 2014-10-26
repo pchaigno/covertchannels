e70f8eca5244f4dac37ff703f493ebd09e50773e
Paul Chaignon <paul.chaignon@gmail.com>
2014-10-26 10:20:24 -0400

    Dump and rebuild a git repository

---
+++ git/git_dump.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import json
+import os
+
+if len(sys.argv) < 2:
+	print("usage: rebuil_git.py <repository>")
+	sys.exit(2)
+
+repository = sys.argv[1]
+os.chdir(repository)
+
+os.system("""git log --reverse --pretty=format:'{%n  "hash": "%H",%n  "author": "%an",%n  "author-email": "%ae",%n  "author-date": %at,%n  "committer": "%cn",%n  "committer-email": "%ce",%n  "committer-date": %ct,%n  "message": "%s"%n},' $@ | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/' > logs.json""")
+
+json_data = open('logs.json')
+logs = json.load(json_data)
+json_data.close()
+
+os.system("git show --binary %s > 0.patch" % (logs[0]['hash']))
+for i in range(1, len(logs)):
+	os.system("git diff --binary %s %s > %d.patch" % (logs[i-1]['hash'], logs[i]['hash'], i))
