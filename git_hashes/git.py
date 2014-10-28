c8fa86cc764b780a5827c255c2e948473771d559
Paul Chaignon <paul.chaignon@gmail.com>
2014-10-28 08:51:51 -0400

    Escape double quotes in JSON document for logs

--- git_hashes/git.py
+++ git_hashes/git.py
@@ -9,6 +9,18 @@ def dump_repository(repository):
 	os.system("""git log --reverse --pretty=format:'{%n  "hash": "%H",%n  "author": "%an",%n  "author-email": "%ae",%n  "author-date": %at,%n  "committer": "%cn",%n  "committer-email": "%ce",%n  "committer-date": %ct,%n  "message": "%s"%n},' $@ | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/' > logs.json""")
 
 	json_data = open('logs.json')
+	json_escaped = open('logs-escaped.json', 'w')
+	for line in json_data:
+		matches = re.match(r'^  "message": "(.+)"$', line)
+		if matches:
+			message = matches.group(1).replace('"', '\\"')
+			line = '  "message": "%s"\n' % (message)
+		json_escaped.write(line)
+	json_data.close()
+	json_escaped.close()
+	os.system("mv logs-escaped.json logs.json")
+
+	json_data = open('logs.json')
 	logs = json.load(json_data)
 	json_data.close()
 
