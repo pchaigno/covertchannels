d20e16dc5e175badc888f7afcef0a9f0a6d3f253
Paul Chaignon <paul.chaignon@gmail.com>
2014-11-05 20:31:56 -0500

    Comments for space encoding module

--- git_hashes/space_encoding.py
+++ git_hashes/space_encoding.py
@@ -2,6 +2,19 @@
 import sys;
 import string;
 
+"""Increments the binary encoding by one.
+
+If the last encoded space string was the maximum, the length of the string is increased.
+Otherwise, the binary value is just increased.
+110 => 111
+111 => 0001
+
+Args:
+	string: The encoded string.
+
+Returns:
+	The new encoded string.
+"""
 def increment(string):
 	new_string = ''
 	all_1 = True
@@ -17,6 +30,18 @@ def increment(string):
 		new_string = '{:0{}b}'.format(int(string, 2) + 1, len(string))
 	return new_string
 
+
+"""Decodes a string of encoded spaces.
+
+Converts the encoded spaces (as binary) into actual spaces and tabulations.
+A 0 becomes a white space and a 1 becomes a tabulation.
+
+Args:
+	string: The spaces encoded in binary.
+
+Returns:
+	The string of spaces and tabulations.
+"""
 def decode(string):
 	spaces = "";
 	for bit in string:
@@ -25,6 +50,7 @@ def decode(string):
 		else:
 			spaces += "	"
 	return spaces
+
 
 if __name__ == "__main__":
 	string = '0'
